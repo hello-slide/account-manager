9c74c2cf919c51da37a12ff5418c2237e121b194
Yuto Watanabe <yuto.w51942@gmail.com>
2021-07-29 02:01:27 +0900

    fix secret store name

--- manager/urls.go
+++ manager/urls.go
@@ -4,7 +4,7 @@ const (
 	USER_DATA_STATE   = "user-data-state"
 	LOGIN_TOKEN_STATE = "login-token-state"
 
-	SECRET_STORE               = "account-manager-secret"
+	SECRET_STORE               = "google-secret-state"
 	GOOGLE_OAUTH_PUBLIC_SECRET = "google-oauth-public"
 	PASETO_COMMON_KEY_SECRET   = ""
 	SEED_SECRET                = "seed-value"
