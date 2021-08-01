725a07751c5b607c1b3b3ead61f8dbe55259f766
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-01 14:59:21 +0900

    change response json key name

--- manager/returnData.go
+++ manager/returnData.go
@@ -1,6 +1,6 @@
 package manager
 
 type ReturnData struct {
-	LoginSession string `json:"login_settion"`
-	Session      string `json:"settion"`
+	LoginSession string `json:"login_token"`
+	Session      string `json:"session_token"`
 }
