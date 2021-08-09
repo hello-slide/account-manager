aa14d2a0aeef5a67e7cc7b5631b068f5536b9643
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-09 16:19:20 +0900

    test: added slideconfig delete logic

--- manager/delete.go
+++ manager/delete.go
@@ -12,5 +12,10 @@ func Delete(ctx *context.Context, client *client.Client, token string) error {
 	if err := loginState.Delete(token); err != nil {
 		return err
 	}
+
+	slideConfigState := state.NewState(client, ctx, "SlideConfig")
+	if err := slideConfigState.Delete(token); err != nil {
+		return err
+	}
 	return nil
 }
