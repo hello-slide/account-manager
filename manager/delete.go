d908ac77b542edc4387eb974cd9c8bbd80a5e34a
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-20 14:40:14 +0900

    change delete logice

--- manager/delete.go
+++ manager/delete.go
@@ -9,7 +9,22 @@ import (
 
 func Delete(ctx *context.Context, client *client.Client, token string) error {
 	loginState := state.NewState(client, ctx, refreshTokenState)
+	userId, err := loginState.Get(token)
+	if err != nil {
+		return err
+	}
+
 	if err := loginState.Delete(token); err != nil {
+		return err
+	}
+
+	userState := state.NewState(client, ctx, userDataState)
+	if err := userState.Delete(string(userId.Value)); err != nil {
+		return err
+	}
+
+	emailState := state.NewState(client, ctx, userEmailState)
+	if err := emailState.Delete(string(userId.Value)); err != nil {
 		return err
 	}
 	return nil
