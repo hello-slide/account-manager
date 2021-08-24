795a1a77336296ce43985581f65c3d4ed98243f3
Yuto Watanabe <yuto.w51942@gmail.com>
2021-08-24 16:56:41 +0900

    changed delete account logic

--- manager/delete.go
+++ manager/delete.go
@@ -2,6 +2,7 @@ package manager
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/dapr/go-sdk/client"
 	"github.com/hello-slide/account-manager/state"
@@ -14,18 +15,27 @@ func Delete(ctx *context.Context, client *client.Client, token string) error {
 		return err
 	}
 
-	if err := loginState.Delete(token); err != nil {
+	userState := state.NewState(client, ctx, userDataState)
+
+	var userData map[string]string
+	_userData, err := userState.Get(string(userId.Value))
+	if err != nil {
 		return err
 	}
+	json.Unmarshal(_userData.Value, &userData)
 
-	userState := state.NewState(client, ctx, userDataState)
 	if err := userState.Delete(string(userId.Value)); err != nil {
 		return err
 	}
 
 	emailState := state.NewState(client, ctx, userEmailState)
-	if err := emailState.Delete(string(userId.Value)); err != nil {
+	if err := emailState.Delete(userData["email"]); err != nil {
 		return err
 	}
+
+	if err := loginState.Delete(token); err != nil {
+		return err
+	}
+
 	return nil
 }
